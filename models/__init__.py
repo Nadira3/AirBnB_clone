692996ef403632c9f67884c5ae0c36c4aca81093
Nadira3 <paitanun35@gmail.com>
2023-10-14 11:24:52 +0000

    models package file

---
+++ models/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" This module contains storage file initialization """
+
+from models import base_model, amenity, city, place, state, user
+from models.engine.file_storage import FileStorage
+
+
+storage = FileStorage()
+storage.reload()
