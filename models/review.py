ac7aefcb4512fa5395074c8f2984f495b814a866
Nadira3 <paitanun35@gmail.com>
2023-10-14 11:26:44 +0000

    review class implementation

---
+++ models/review.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+    review module
+"""
+
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """
+        Review class for other classes
+    """
+
+    place_id = ""
+    user_id = ""
+    text = ""
+
+    def __init__(self, *args, **kwargs):
+        """
+            initializes the instance of an object
+        """
+        super().__init__(*args, **kwargs)
