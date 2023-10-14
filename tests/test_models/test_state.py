e48ce471e920ef65464d7db9a8160e81ee024d68
Nadira3 <paitanun35@gmail.com>
2023-10-14 11:28:38 +0000

    tests files upload to run checker

---
+++ tests/test_models/test_state.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" Test Cases for State """
+from unittest import TestCase
+from tests.test_models import test_base_model
+from models.state import State
+
+
+class TestState_extend_basemodel(test_base_model.BaseModelTestCases):
+    """ Test Cases for State """
+
+    def setUp(self):
+        """ Set up """
+        super().setUp()
+        self.state = State()
+
+    def test_state_to_dict(self):
+        """ Test State to dict """
+        self.state.name = 'Snake land'
+        state_obj = self.state.to_dict()
+        self.assertTrue(self.state.name in state_obj.values())
+
+
+class TestState(TestCase):
+    """ Test State """
+
+    def test_state_name(self):
+        """ Test State name """
+        new_state = State()
+        self.assertIsInstance(new_state.name, str)
